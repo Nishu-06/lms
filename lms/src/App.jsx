9b5499381e0c0e7f5b05deb5f1eb6bb0a59810a5
crysttwitch <147266137+crysttwitch@users.noreply.github.com>
2024-07-08 22:06:58 +0530

    change

--- lms/src/App.jsx
+++ lms/src/App.jsx
@@ -11,8 +11,22 @@ import axios from 'axios';
 import DeleteBook from './components/DeleteBook';
 
 function App() {
-    const [role, setRole] = useState('');
+    const [role, setRole] = useState('')
+
+    axios.defaults.withCredentials = true;
+    useEffect(() => {
+    axios.get('http://localhost:3001/auth/verify')
+    .then(res => {
+    if(res.data.login) {
+    setRole(res.data.role)
+    } else {
+    setRole('')
+    }
+    console.log(res)
+    }).catch(err=> console.log(err))
+    }, [])
     return (
+    
         <BrowserRouter>
             <Navbar role={role} />
             <Routes>
